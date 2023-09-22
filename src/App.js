e5bb69d4b42af42956c8453acfc4b86dc975ae5d
Tobias Ahrens <tobias-ahrens@hotmail.de>
2023-09-22 12:33:04 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import "./App.css";
-import inputShortener from "./inputShortener";
-import input from "./input";
+import BackgroundAnimate from "./BackgroundAnimate";
+import InputShortener from "./inputShortener";
+import LinkResult from "./LinkResult";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+
   return (
-    <div className="App">
-      <inputShortener />
-      <input />
+    <div className="container">
+      <InputShortener setInputValue={setInputValue} />
+      <BackgroundAnimate />
+      <LinkResult inputValue={inputValue} />
     </div>
   );
 }
--- src/inputShortener.js
+++ src/inputShortener.js
@@ -1,5 +1,29 @@
-const inputShortener = () => {
-  return <div>inputShortener</div>;
+import { useState } from "react";
+
+const InputShortener = ({ setInputValue }) => {
+  const [value, setValue] = useState("");
+
+  const handleClick = () => {
+    setInputValue(value);
+    setValue("");
+  };
+
+  return (
+    <div className="inputContainer">
+      <h1>
+        URL <span>Shrinker</span>
+      </h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter your URL"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button onClick={handleClick}>shrink</button>
+      </div>
+    </div>
+  );
 };
 
-export default inputShortener;
+export default InputShortener;
